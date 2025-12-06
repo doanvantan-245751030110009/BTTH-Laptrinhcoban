339dae928537b5583d757a11f1b6409f471b9c06
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-06 20:52:10 +0700

    CODE

---
+++ BAITH1/4/1.4.py
@@ -0,0 +1,11 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("4)")
+a=int(input("Nhập a:"))
+b=int(input("Nhập b:"))
+i=1
+for j in range(a,b):
+    print("i:",i,"j:",j)
+    print(i,"/",j)
+    print(i/j);
