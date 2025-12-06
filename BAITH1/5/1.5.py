301afb8b775d6ae1e415eea5fdb2d0662499d7eb
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-06 20:55:48 +0700

    CODE

---
+++ BAITH1/5/1.5.py
@@ -0,0 +1,8 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("5)")
+n=int(input("Nhập số tự nhiên n:"))
+while n>=0:
+          print (n);
+          n=n-1;
