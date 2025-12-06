3134eb51a7119e60e69ac3b448389cee4042a4b5
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-06 20:40:08 +0700

    CODE

---
+++ BAITH1/3/1.3.py
@@ -0,0 +1,9 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("3)")
+n=int(input("Nhập số:"))
+if n%2==0:
+    print("Số chẵn");
+else:
+    print("Số lẻ");
