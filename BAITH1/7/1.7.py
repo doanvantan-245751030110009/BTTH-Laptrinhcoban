5a6f717d829ac3262f1771216a9f3b363ebcf132
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-06 21:01:18 +0700

    CODE

---
+++ BAITH1/7/1.7.py
@@ -0,0 +1,9 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("7)")
+n=int(input("Nhập vào một số n:"))
+d=dict()
+for i in range(1,n+1):
+   d[i]=i*i
+print(d)
