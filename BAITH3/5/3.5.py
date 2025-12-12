66df2569824807ca5a30bcf0da9cb23dd13f5249
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:34:01 +0700

    CODE

---
+++ BAITH3/5/3.5.py
@@ -0,0 +1,8 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("5)")
+danhsach = input("Danh sách:").split()
+danhsachnguoc = danhsach[::-1]
+for so in danhsachnguoc:
+    print(so)
