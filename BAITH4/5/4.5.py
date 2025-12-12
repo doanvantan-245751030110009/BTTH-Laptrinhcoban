09b2855d5ec85b59cdeeb34f5594b77ac98e16d4
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:14:33 +0700

    CODE

---
+++ BAITH4/5/4.5.py
@@ -0,0 +1,8 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("5)")
+class py_solution:
+    def reverse_words(self, s):
+        return ' '.join(reversed(s.split()))
+print(py_solution().reverse_words("hello .py"))
