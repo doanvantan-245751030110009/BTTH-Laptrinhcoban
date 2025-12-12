5b452cf9fcf1c2c1a8d99285f0f20a02e9802085
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:36:03 +0700

    CODE

---
+++ BAITH3/6/3.6.py
@@ -0,0 +1,14 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("6)")
+x= input("Nhập họ và tên đầy đủ (ví dụ: Nguyen Van A): ")
+y = x.split()
+if len(y) >= 2:
+    ho = y[0]
+    ten = y[-1]
+    print("\n--- Kết Quả ---")
+    print(f"Họ: {ho}")
+    print(f"Tên riêng: {ten}")
+else:
+    print("Vui lòng nhập họ và tên hợp lệ (có ít nhất 2 từ).")
