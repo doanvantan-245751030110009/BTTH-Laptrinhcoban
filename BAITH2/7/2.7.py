ace0642a8a300d29d5c52ee116af8bb916b2bc0b
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 17:34:57 +0700

    CODE

---
+++ BAITH2/7/2.7.py
@@ -0,0 +1,11 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("7)")
+n=int(input("Nhap so:"))
+def Kiem_tra_so(n):
+ if n%2 == 0:
+  print ("Day là so chan")
+ else:
+  print ("Day la so le")
+Kiem_tra_so(n)
