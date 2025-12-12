d04748c707c799a8021b11e356d2834f5ac73772
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-13 00:00:45 +0700

    CODE

---
+++ BAITH3/C24/3.24.py
@@ -0,0 +1,16 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("24)")
+print("Vui lòng nhập một câu:")
+input_string = input()
+upper_count = 0
+lower_count = 0
+for char in input_string:
+    if char.isupper():
+        upper_count += 1
+    elif char.islower():
+        lower_count += 1
+print(f"Chữ hoa: {upper_count}")
+print(f"Chữ thường: {lower_count}")
+
