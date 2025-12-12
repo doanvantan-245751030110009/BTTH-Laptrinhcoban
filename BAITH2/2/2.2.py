a6a643cf49d1e4c97f5298fc630038720bf16abf
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 17:25:25 +0700

    CODE

---
+++ BAITH2/2/2.2.py
@@ -0,0 +1,8 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("2)")
+def sum(a,b):
+    return a+b
+c=sum(4,5)
+print("Tong cua 4 va 5 = " +str(c))
