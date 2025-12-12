180ee706389bbc47fc9e82ccb5f5ba1d41408c4e
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:04:12 +0700

    CODE

---
+++ BAITH4/6/4.6.py
@@ -0,0 +1,15 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("6)")
+class IOString:
+    def __init__(self):      
+        self.str1 = "" 
+    def get_String(self):
+        self.str1 = input("Nhập chuỗi của bạn: ")
+    def print_String(self):
+        print(self.str1.upper())
+if __name__ == "__main__":
+    str1_instance = IOString()
+    str1_instance.get_String()
+    str1_instance.print_String()
