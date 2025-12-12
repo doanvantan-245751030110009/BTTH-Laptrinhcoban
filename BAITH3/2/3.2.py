30a44ac90c914f47a4a7411160b61984f774a89f
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:24:34 +0700

    CODE

---
+++ BAITH3/2/3.2.py
@@ -0,0 +1,9 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("2)")
+S = input("Nhập chuỗi:")
+for ch in S:
+    if ch == ' ' or ch == '\t':
+        continue
+    print(ch)
