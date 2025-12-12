26efd2965d4fbeebf24a84413889e0c047953d76
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:45:59 +0700

    CODE

---
+++ BAITH3/C15/3.15.py
@@ -0,0 +1,9 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("15)")
+day = input("Nhập dãy các từ tiếng Anh (cách nhau bởi dấu cách): ").split()
+day.sort() 
+print("Các từ sau khi sắp xếp theo thứ tự từ điển:")
+for i in day:
+    print(i)
