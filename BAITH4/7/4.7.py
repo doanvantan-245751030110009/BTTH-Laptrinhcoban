86bff0a4a65502474dd3df8433a97a8705fad04a
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:05:37 +0700

    CODE

---
+++ BAITH4/7/4.7.py
@@ -0,0 +1,16 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("7)")
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+c = Circle(5)
+print("Diện tích:", c.area())
+print("Chu vi:", c.circumference())
