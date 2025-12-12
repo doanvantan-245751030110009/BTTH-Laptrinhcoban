cf92b97c5f0e95be93d15bec1bd8e8aae070a2f8
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 17:29:15 +0700

    CODE

---
+++ BAITH2/4/2.4.py
@@ -0,0 +1,10 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("4)")
+a = "Hello Guy!"
+def say(a):
+ a = "Vinh University"
+ print(a)
+say(a)
+print(a)
