c2153953ec1b037851d0e2d8b13676e55f5fa4a4
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-12 23:32:28 +0700

    CODE

---
+++ BAITH3/4/3.4.py
@@ -0,0 +1,8 @@
+print("Đoàn Văn Tân")
+print("MSV: 045751030110009")
+print("################################")
+print("4)")
+danhsach = input("Danh sách:").split()
+print(danhsach)
+for so in danhsach:
+    print(so)
