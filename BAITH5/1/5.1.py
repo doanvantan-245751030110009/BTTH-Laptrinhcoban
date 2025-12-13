880f944e7083961ac265865680ba95294cd3c59f
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-13 09:04:43 +0700

    CODE

---
+++ BAITH5/1/5.1.py
@@ -0,0 +1,15 @@
+#245751030110009-
+import turtle
+def draw_square():
+    screen = turtle.Screen()
+    screen.setup(width=400, height=400)
+    pen = turtle.Turtle()
+    pen.shape("turtle")
+    pen.color("blue")
+    pen.speed(1)
+    for _ in range(4):
+        pen.forward(100) 
+        pen.right(90)  
+    screen.mainloop()
+if __name__ == "__main__":
+    draw_square()
