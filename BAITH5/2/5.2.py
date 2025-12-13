f1eabbd0e38bba52015ac30c5224e713f3100c63
doanvantan-245751030110009 <tanvandoan254@gmail.com>
2025-12-13 09:03:54 +0700

    CODE

---
+++ BAITH5/2/5.2.py
@@ -0,0 +1,12 @@
+#245751030110009-
+import turtle
+def draw_circle():
+    screen = turtle.Screen()
+    screen.setup(width=400, height=400)
+    pen = turtle.Turtle()
+    pen.color("red")
+    pen.speed(3)
+    pen.circle(50)
+    screen.mainloop()
+if __name__ == "__main__":
+    draw_circle()
